528c18199a64484c31c3d3fee5a24f026d5deb16
Arturo <arturo@kunder.cl>
2013-11-21 11:36:58 -0300

    insta videos

--- public/js/kunder.instagram.posts.js
+++ public/js/kunder.instagram.posts.js
@@ -4,17 +4,34 @@ kunder.Instagram.Posts = (function($) {
 	
 	function _initialize() {
 		$(document).ready(function() {
-			console.log('hola');
 			$('img.load').each(function(i, item){
 				$(item).attr('src', $(item).data('src'));
 				$(item).removeClass('load');
-			})
+			});
 		});
 		
+		$(document).on('click', '.post .picture.is-video', _playVideo);
 		
 	}
 	
-
+	function _playVideo(event) {
+		event.preventDefault();
+		
+		var picture = $(this);
+		var video = picture.parent().find('video').first();
+		console.log(video);
+		video.attr('src', video.data('src'));
+		video[0].load();
+		
+		video.on('ended', function(){
+			video.parent().hide();
+			picture.show();
+		});
+		
+		picture.hide();
+		video.parent().show();
+		video[0].play();
+	}
 	
 	return {
 		Initialize :	_initialize
